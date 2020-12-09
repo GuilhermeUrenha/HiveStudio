fd0709f881c8b6762da3697c624f3d19a2271361
GuilhermeUrenha <guilherme.urenha@gmail.com>
2020-12-09 18:23:01 -0300

    Models fim

--- HiveStudio/Models/Orcamento.cs
+++ HiveStudio/Models/Orcamento.cs
@@ -1,11 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HiveStudio.Models
 {
     public class Orcamento
     {
+        [Key]
+        [Display(Name = "Código de Cidade")]
+        public int idParcela { get; set; }
+
+        [ForeignKey("PagamentoMetodo")]
+        [Display(Name = "Código do Método")]
+        public int idMetodo { get; set; }
+
+        [Display(Name = "Preço Total")]
+        [Required(ErrorMessage = "O campo é obrigatório!")]
+        public decimal precoTotal { get; set; }
+
+        [Display(Name = "Estado de pagamento")]
+        [Required(ErrorMessage = "O campo é obrigatório!")]
+        public int pago { get; set; }
+
+        [Display(Name = "Data de Pagamento")]
+        public DateTime dataPgto { get; set; }
+
+        [Display(Name = "Data de Vencimento")]
+        public DateTime dataVenc { get; set; }
     }
 }
--- HiveStudio/Models/Usuario.cs
+++ HiveStudio/Models/Usuario.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.SqlTypes;
-using System.Linq;
-using System.Web;
-using MySql.Data.Types;
 
 namespace HiveStudio.Models
 {
