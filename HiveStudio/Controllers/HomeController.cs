f3d2097f91fbd0e64b9a6efdfd2e4b42e7ba9ef8
reptileeee <65796757+reptileeee@users.noreply.github.com>
2020-12-10 04:25:28 -0300

    Contato pronto, Layout1 e 2 faltando links (img) registro removido layouts

--- HiveStudio/Controllers/HomeController.cs
+++ HiveStudio/Controllers/HomeController.cs
@@ -20,20 +20,19 @@ namespace HiveStudio.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        
         public ActionResult Portfolio()
         {
 
             return View();
         }
 
-        public ActionResult Login()
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contato()
         {
 
             return View();
