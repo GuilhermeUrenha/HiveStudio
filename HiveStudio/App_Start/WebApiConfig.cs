642a1f3f65c81cdc7f1619fbdbf56b46618a32e4
GuilhermeUrenha <guilherme.urenha@gmail.com>
2020-12-10 06:24:04 -0300

    Identity WebApiConfig

--- HiveStudio/App_Start/WebApiConfig.cs
+++ HiveStudio/App_Start/WebApiConfig.cs
@@ -6,8 +6,12 @@ namespace HiveStudio
     {
         public static void Register(HttpConfiguration configuration)
         {
-            configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
-              new { id = RouteParameter.Optional });
+            configuration.MapHttpAttributeRoutes();
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultApi", 
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
         }
     }
 }
